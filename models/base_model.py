9c87a57cc494886e7bbae7d27ca3c88cf5b3743f
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 14:39:29 -0800

    task 1

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+import uuid
+
+
+class Basemodel:
+    def __init__(slef, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4()):wq
+
