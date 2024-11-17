6c9695547716a644428ba3045c14412f924583fc
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 15:29:49 -0800

    crr str

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,12 @@ class BaseModel:
                     val = datetime.fromisoformat(val)
                 setattr(self, key, val)
 
-        elseiiii:
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    def __ste__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {seld.__dict__}"
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     def save(self):
         self.updated_at = datetime.now()
     def to_dict(self):
