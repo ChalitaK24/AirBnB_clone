fde7abe45561c0cc08b131cd0b5fa440fa773cc9
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 14:52:57 -0800

    datetime

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 
 import uuid
 
+from datetime import datetime
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -11,3 +12,5 @@ class BaseModel:
 
         else:
             self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
