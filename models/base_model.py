101f0269a5b25c71ee10705d4a2aa41b24dbd259
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 15:22:16 -0800

    crr

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,12 @@ from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.fromisoformat(val)
+                setattr(self, key, val)
 
-        else:
+        elseiiii:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
