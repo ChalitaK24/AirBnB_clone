33f70c552d5cca44c5a1debc2da90e9ee1dd3130
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 15:09:15 -0800

    dict

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+    def __ste__(self):
+        return f"[{self.__class__.__name__}] (self.id) {seld.__dict__}"
     def save(self):
-        self.updated.et = datetime.now()
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        res["created_at"] = self.created_at.isoformat()
+        res["updated_at"] =self.updated_at.isoformat()
+
+        return res
