9ae3ffee345f6340db0b332406bbf0a78fe34e87
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 15:49:19 -0800

    crr pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
@@ -16,10 +17,13 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         self.updated_at = datetime.now()
+
     def to_dict(self):
         res = self.__dict__.copy()
         res["__class__"] = self.__class__.__name__
