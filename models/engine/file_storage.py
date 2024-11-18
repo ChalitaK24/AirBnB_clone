b05d2ff15d41ca189ebbd89e0694caf6fa3775e0
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 18:28:21 -0800

    sv crr

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,6 +28,14 @@ class FileStorage:
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
+    def save(self):
+        """
+        serializes __object to JSON file
+        """
+        obj_dict = {key: obj.to_dict() for key, obj in self. _objects.items()}
+        with open(self.__file_path, "w") as file:
+            json.dump(obj_dict, file)
+
     def reload_des(self):
         """
         deserilaization
