9966911bf13a148f86e1f282ff04a549aee34530
ChalitaK24 <schalitakel@gmail.com>
2024-11-17 16:31:02 -0800

    docstr

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+BaseModel class provides
+common attr and mtds to be inherited by other classes
+"""
 
 import uuid
 
@@ -6,7 +10,16 @@ from datetime import datetime
 
 
 class BaseModel:
+    """
+    Defines all common attributes/methods for other classes.
+    """
     def __init__(self, *args, **kwargs):
+        """
+        new Base model instance initialize
+        Args:
+            *args: unused
+            **kwargs: Key value pairs
+        """
         if kwargs:
             for key, val in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -19,12 +32,27 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        String representation of an instance
+
+        Returns: str: Formatted string
+        representation of the instance
+        """
+
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+        Updates the public instance attribute
+        `updated_at` with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary containing
+        all keys/vals of the instance's __dict__
+        """
         res = self.__dict__.copy()
         res["__class__"] = self.__class__.__name__
         res["created_at"] = self.created_at.isoformat()
