630d4e1cccba22ea1977838583b4ecebde844536
ChalitaK24 <schalitakel@gmail.com>
2024-11-19 07:03:14 -0800

    consle

--- console.py
+++ console.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
+""" command interpreter entry point"""
 
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    cmd interpreter for HBNB
+    """
+
+    prompt = "(hbng) "
+
+    def do_quit(self, arg):
+        return True
+
+    def EOD(self, arg):
+        print("")
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
