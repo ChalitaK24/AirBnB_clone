992040462d3dc38434158b47fa884a2b00454276
ChalitaK24 <schalitakel@gmail.com>
2024-11-19 07:10:03 -0800

    consolecrr

--- console.py
+++ console.py
@@ -14,9 +14,14 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         return True
 
-    def EOD(self, arg):
+    def EOF_do(self, arg):
+
         print("")
         return True
+
+    def emptyline(self):
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
